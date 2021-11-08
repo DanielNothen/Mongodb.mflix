1850b3dd369d063f72c3f51c4b0ff60686501f2e
DanielNothen <74382615+DanielNothen@users.noreply.github.com>
2021-11-08 14:37:25 -0600

    Add files via upload

---
+++ test/config/mongoEnvironment.js
@@ -0,0 +1,31 @@
+const MongoClient = require("mongodb").MongoClient
+const NodeEnvironment = require("jest-environment-node")
+module.exports = class MongoEnvironment extends NodeEnvironment {
+  async setup() {
+    if (!this.global.mflixClient) {
+      this.global.mflixClient = await MongoClient.connect(
+        "mongodb+srv://m220student:m220password@mflix.agewg.mongodb.net/M220JS",
+        // TODO: Connection Pooling
+        // Set the connection pool size to 50 for the testing environment.
+        // TODO: Timeouts
+        // Set the write timeout limit to 2500 milliseconds for the testing environment.
+        {
+          db: { wtimeout: 2500 },
+          useNewUrlParser: true,
+          poolSize: 50,
+          tls: true,
+        }
+      )
+      await super.setup()
+    }
+  }
+
+  async teardown() {
+    await this.global.mflixClient.close()
+    await super.teardown()
+  }
+
+  runScript(script) {
+    return super.runScript(script)
+  }
+}
